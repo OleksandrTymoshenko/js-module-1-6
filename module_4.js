f195611c97285534eb66f097934cb5d06d80cca9
OleksandrTymoshenko <nomakievip@gmail.com>
2021-11-14 21:41:34 +0200

    Update module_4.js

--- module_4.js
+++ module_4.js
@@ -1,2 +1,99 @@
 // Задача 1
 
+// Функция как значение
+// Функции не отличаются от чисел, строк или массивов - это просто специальный тип данных (объект высшего порядка), значение, которое можно хранить в переменной или передавать, как аргумент, в другую функцию.
+
+// function greet(name) {
+//   return `Добро пожаловать ${name}.`;
+// }
+
+// // Вызываем функцию greet
+// // и выводим результат в консоль
+// console.log(greet("Mango"));
+// // Добро пожаловать Mango.
+
+// // Выводим функцию greet
+// // в консоль не вызывая её
+// console.log(greet);
+// /*
+// ƒ greet() {
+//      return `Добро пожаловать ${name}.`;
+// }
+// */
+// В первом логе мы вызываем функцию greet при помощи круглых скобок и в консоль выводится результат её выполнения. Во втором логе передаётся ссылка на функцию, а не результат её вызова (отсутствуют круглые скобки), поэтому в консоль выводится тело функции. Это значит, что функцию можно присвоить в переменную или передать, как аругмент, другой функции.
+
+// Задание
+// Дополни код так, чтобы в переменной result был результат выполнения функции makePizza, а в переменной pointer была ссылка на функцию makePizza.
+
+// Тесты
+// Объявлена функция makePizza
+// Объявлена переменная result
+// Значение переменной result это строка "Your pizza is being prepared, please wait."
+// Значение переменной result получено с помощью вызова функции
+// Объявлена переменная pointer
+// Значение переменной pointer это ссылка на функцию makePizza
+
+function makePizza() {
+  return "Your pizza is being prepared, please wait.";
+}
+// Change code below this line
+
+const result = null;
+const pointer = null;
+
+
+// Решение
+
+// function makePizza() {
+//   return "Your pizza is being prepared, please wait.";
+// }
+// const result = makePizza(); 
+// const pointer = makePizza;
+
+// // Задача 2
+
+// Колбэк-функции
+// Функция обратного вызова (callback, колбэк) - это функция, которая передаётся другой функции в качестве аргумента и та, в свою очередь, вызывает переданную функцию.
+
+// Функция высшего порядка(higher order function) - функция, принимающая в качестве параметров другие функции или возвращающая функцию как результат.
+
+// // Колбэк-функция
+// function greet(name) {
+//   consle.log(`Добро пожаловать ${name}.`);
+// }
+
+// // Функция высшего порядка
+// function registerGuest(name, callback) {
+//   console.log(`Регистрируем гостя ${name}.`);
+//   callback(name);
+// }
+
+// registerGuest("Mango", greet);
+// Мы передали ссылку на функцию greet как аргумент, поэтому она будет присвоена в параметр callback и вызвана внутри функции registerGuest через круглые скобки. Имя параметра для колбэка может быть произвольным, главное помнить, что значением будет функция.
+
+// Задание
+// Дополни функцию makeMessage так, чтобы она ожидала вторым параметром (параметр callback) колбэк-функцию и возвращала ее вызов. Функция deliverPizza или makePizza будет передаваться как колбэк и ожидать аргументом имя готовой доставляемой пиццы.
+
+// Тесты
+// Объявлена функция deliverPizza
+// Объявлена функция makePizza
+// Объявлена функция makeMessage
+// Функция makeMessage принимает два параметра, названые согласно задания, pizzaName и callback
+// Вызов makeMessage("Royal Grand", makePizza) возвращает строку "Pizza Royal Grand is being prepared, please wait..."
+// Вызов makeMessage("Ultracheese", deliverPizza) возвращает строку "Delivering Ultracheese pizza."
+
+function deliverPizza(pizzaName) {
+  return `Delivering ${pizzaName} pizza.`;
+}
+
+function makePizza(pizzaName) {
+  return `Pizza ${pizzaName} is being prepared, please wait...`;
+}
+
+// Chande code below this line
+function makeMessage(pizzaName) {
+  return;
+}
+
+// Решение
+
