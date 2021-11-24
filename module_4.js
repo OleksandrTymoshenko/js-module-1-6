b6b3ccf3936b51da4f3c51089b160c15b9baca0d
OleksandrTymoshenko <nomakievip@gmail.com>
2021-11-24 15:10:37 +0200

    Update module_4.js

--- module_4.js
+++ module_4.js
@@ -323,3 +323,205 @@ function calculateTotalPrice(orderedItems) {
 
 // Решение
 
+function calculateTotalPrice(orderedItems) {
+  let totalPrice = 0;
+  // Change code below this line
+
+
+  orderedItems.forEach(iteam => {
+      totalPrice += iteam});
+  return totalPrice;
+}
+
+// Задача 6
+
+// Задача. Фильтрация массива чисел
+// Задание
+// Функция filterArray(numbers, value) принимает массив чисел numbers и возвращает новый массив, в котором будут только те элементы оригинального массива, которые больше чем значение параметра value.
+
+// Выполни рефакторинг функции так, чтобы вместо цикла for она использовала метод forEach.
+
+// Тесты
+// Объявлена функция filterArray(numbers, value)
+// Для перебора массива numbers использован метод forEach
+// Вызов функции filterArray([1, 2, 3, 4, 5], 3) возвращает [4, 5]
+// Вызов функции filterArray([1, 2, 3, 4, 5], 4) возвращает [5]
+// Вызов функции filterArray([1, 2, 3, 4, 5], 5) возвращает []
+// Вызов функции filterArray([12, 24, 8, 41, 76], 38) возвращает [41, 76]
+// Вызов функции filterArray([12, 24, 8, 41, 76], 20) возвращает [24, 41, 76]
+// Вызов функции со случайными, но валидными аргументами, возвращает правильное значение
+
+function filterArray(numbers, value) {
+  const filteredNumbers = [];
+  // Change code below this line
+
+  for (let i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] > value) {
+      filteredNumbers.push(numbers[i]);
+    }
+  }
+
+  // Change code above this line
+  return filteredNumbers;
+}
+
+// Решение
+
+function filterArray(numbers, value) {
+  const filteredNumbers = [];
+  // Change code below this line
+
+  numbers.forEach(item => {
+    if(item > value) {
+      filteredNumbers.push(item);
+    }
+  });
+
+  // Change code above this line
+  return filteredNumbers;
+}
+
+// Задача 7
+
+// Задача. Общие элементы
+// Задание
+// Функция getCommonElements(firstArray, secondArray) принимает два массива произвольной длины в параметры firstArray и secondArray, и возвращает новый массив их общих элементов, то есть тех которые есть в обоих массивах.
+
+// Выполни рефакторинг функции так, чтобы вместо цикла for она использовала метод forEach.
+
+// Тесты
+// Объявлена функция getCommonElements(firstArray, secondArray)
+// Для перебора параметра (массива) использован метод forEach
+// Вызов getCommonElements([1, 2, 3], [2, 4]) возвращает [2]
+// Вызов getCommonElements([1, 2, 3], [2, 1, 17, 19]) возвращает [1, 2]
+// Вызов getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]) возвращает [12, 27, 3]
+// Вызов getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]) возвращает [10, 30, 40]
+// Вызов getCommonElements([1, 2, 3], [10, 20, 30]) возвращает []
+// Вызов функции со случайными, но валидными аргументами, возвращает правильное значение
+
+function getCommonElements(firstArray, secondArray) {
+  const commonElements = [];
+  // Change code below this line
+
+  for (let i = 0; i < firstArray.length; i += 1) {
+    if (secondArray.includes(firstArray[i])) {
+      commonElements.push(firstArray[i]);
+    }
+  }
+
+  return commonElements;
+  // Change code above this line
+}
+
+// Решение
+
+function getCommonElements(firstArray, secondArray) {
+  const commonElements = [];
+  firstArray.forEach(number => {
+    if (secondArray.includes(number)) {
+      commonElements.push(number);
+    }
+  });
+  return commonElements;
+}
+
+// Задача 8
+
+// Стрелочные функции.
+// Стрелочные функции имеют сокращённый, более лаконичный синтаксис, что уменьшает объем кода, особенно когда функция маленькая или если она используется как коллбек.
+
+// Все стрелки создаются как функциональное выражение, и если функция не анонимна, то она должна быть присвоена переменной.
+
+// // Обычное объявление функции
+// function classicAdd(a, b, c) {
+//   return a + b + c;
+// }
+
+// // Тоже самое как стрелочная функция
+// const arrowAdd = (a, b, c) => {
+//   return a + b + c;
+// };
+// Ключевое слово function не используется, вместо этого сразу идёт объявление параметров, за которыми следует символ => и тело функции.
+
+// Если параметров несколько, то они перечисляются через запятую в круглых скобках, между знаками равно = и стрелкой =>.
+
+// const add = (a, b, c) => {
+//   return a + b + c;
+// };
+// Если параметр один, его объявление может быть без круглых скобок.
+
+// const add = a => {
+//   return a + 5;
+// };
+// Если параметров нет, то обязательно должны быть пустые круглые скобки.
+
+// const greet = () => {
+//   console.log("Привет!");
+// };
+// Задание
+// Выполни рефакторинг функции calculateTotalPrice() так, чтобы она была объявлена как стрелочная.
+
+// Тесты
+// Объявлена переменная calculateTotalPrice
+// Переменной calculateTotalPrice присвоена стрелочная функция с параметрами (quantity, pricePerItem)
+// Вызов calculateTotalPrice(5, 100) возвращает 500
+// Вызов calculateTotalPrice(8, 60) возвращает 480
+// Вызов calculateTotalPrice(3, 400) возвращает 1200
+// Вызов функции со случайными, но валидными аргументами, возвращает правильное значение
+
+// Change code below this line
+
+function calculateTotalPrice(quantity, pricePerItem) {
+  // Change code above this line
+  return quantity * pricePerItem;
+}
+
+// Решение
+
+  const calculateTotalPrice = (quantity, pricePerItem) => {
+  return quantity * pricePerItem;
+}
+
+// Задача 9
+
+// Неявный возврат
+// В стрелочной функции после символа => идёт её тело. Здесь может быть два варианта: с фигурными скобками и без них.
+
+// const add = (a, b, c) => {
+//   console.log(a, b, c);
+//   return a + b + c;
+// };
+// Если фигурные скобки есть, и функция должна возвращать какое-то значение, необходимо явно поставить return. Это называется явный возврат (explicit return). Такой синтаксис используется в том случае, если в теле функции нужно выполнить ещё какие-то инструкции кроме возврата значения.
+
+// const add = (a, b, c) => a + b + c;
+// Если фигурных скобок нет, то возвращается результат выражения стоящего после =>. Это называется неявный возврат (implicit return). В примере вернётся результат выражения сложения параметров a, b и c.
+
+// Синтаксис неявного возврата сильно сокращает «шум» объявления функции с телом и возвращаемым выражением, но подходит только в случае когда в теле функции не нужно выполнять никаких дополнительных инструкций кроме возврата значения.
+
+// // До
+// function classicAdd(a, b, c) {
+//   return a + b + c;
+// }
+
+// // После
+// const arrowAdd = (a, b, c) => a + b + c;
+// Задание
+// Выполни рефакторинг функции calculateTotalPrice() так, чтобы она использовала неявный возврат.
+
+// Тесты
+// Объявлена переменная calculateTotalPrice
+// Переменной calculateTotalPrice присвоена стрелочная функция с параметрами (quantity, pricePerItem)
+// В функции использован неявный возврат
+// Вызов calculateTotalPrice(5, 100) возвращает 500
+// Вызов calculateTotalPrice(8, 60) возвращает 480
+// Вызов calculateTotalPrice(3, 400) возвращает 1200
+// Вызов функции со случайными, но валидными аргументами, возвращает правильное значение
+
+// Change code below this line
+const calculateTotalPrice = (quantity, pricePerItem) => {
+  return quantity * pricePerItem;
+};
+// Change code above this line
+
+// Решение
+
