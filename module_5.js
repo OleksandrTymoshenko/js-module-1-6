ffd0be9da1c04db0eb29243952ad1b600c9ef6f3
OleksandrTymoshenko <nomakievip@gmail.com>
2021-11-29 23:57:44 +0200

    5 модуль полностью

--- module_5.js
+++ module_5.js
@@ -1301,3 +1301,45 @@ mango.blacklist("poly@mail.com");
 console.log(mango.blacklistedEmails); // ["poly@mail.com"]
 console.log(mango.isBlacklisted("mango@mail.com")); // false
 console.log(mango.isBlacklisted("poly@mail.com")); // true
+
+// Решение
+
+class User { 
+  email; 
+  constructor(email) { 
+    this.email = email; 
+  } 
+  get email() { 
+    return this.email; 
+  } 
+  set email(newEmail) { 
+    this.email = newEmail; 
+  } 
+} 
+class Admin extends User { 
+  static AccessLevel = { 
+    BASIC: "basic", 
+    SUPERUSER: "superuser", 
+  }; 
+  constructor({ email, accessLevel, blacklistedEmails }) { 
+    super(email); 
+    this.accessLevel = accessLevel; 
+    this.blacklistedEmails = []; 
+  } 
+  blacklist(email) { 
+    this.blacklistedEmails.push(email); 
+  } 
+  isBlacklisted(email) { 
+    return this.blacklistedEmails.includes(email); 
+  } 
+} 
+const mango = new Admin({ 
+  email: "mango@mail.com", 
+  accessLevel: Admin.AccessLevel.SUPERUSER, 
+}); 
+console.log(mango.email); // "mango@mail.com" 
+console.log(mango.accessLevel); // "superuser" 
+mango.blacklist("poly@mail.com"); 
+console.log(mango.blacklistedEmails); // ["poly@mail.com"] 
+console.log(mango.isBlacklisted("mango@mail.com")); // false 
+console.log(mango.isBlacklisted("poly@mail.com")); // true
